10f9e90c8f0a658eb288df21aad44c5e6ba5b37d
konst <konstantin.mikheev@icord.cz>
2020-11-27 14:05:36 +0100

    errors

--- server/src/utils/errors.ts
+++ server/src/utils/errors.ts
@@ -1,5 +1,7 @@
-export const errMap = (error: any) =>
-  JSON.stringify(error, [
+import * as sentry from '@sentry/node';
+
+export const errMap = (error: any) => {
+  const message = JSON.stringify(error, [
     'message',
     'arguments',
     'type',
@@ -9,3 +11,11 @@ export const errMap = (error: any) =>
     'statusCode',
     'stack',
   ]);
+
+  sentry.captureEvent({
+    message,
+    level: sentry.Severity.Error,
+  });
+
+  return message;
+};
