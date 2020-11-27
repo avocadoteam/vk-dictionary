37527b7a3efab16689705638f81cd748cd9bf156
konst <konstantin.mikheev@icord.cz>
2020-11-27 14:05:43 +0100

    bump vers

--- client/src/core/models/constants.ts
+++ client/src/core/models/constants.ts
@@ -1,4 +1,4 @@
-export const appV = '1.1';
+export const appV = '1.2';
 
 export const vkApiV = '5.124';
 
--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -2,9 +2,7 @@ export const oneMinute = 60000;
 export const ASKTM = oneMinute;
 export const ASKTHM = oneMinute / 2 - 1;
 export const vkApiV = '5.124';
-export const int4 = 2147483647;
-export const appV = '1.1';
-export const groupId = 6666666;
+export const appV = '1.2';
 export const appId = 7661160;
 
 export const oneMillion = 1000000;
