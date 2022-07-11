6c87d8a18862aab1674dcb31a984570b3b020830
Konstantin <konstantin.miheev@sensearena.com>
2022-07-11 14:05:26 +0200

    cast any

--- client/src/core/vk-bridge/ads.ts
+++ client/src/core/vk-bridge/ads.ts
@@ -1,5 +1,5 @@
 import { vkBridge } from './instance';
 
-export const getAdsData = vkBridge.supports('VKWebAppGetAds')
+export const getAdsData = vkBridge.supports('VKWebAppGetAds' as any)
   ? vkBridge.send('VKWebAppGetAds' as any)
   : Promise.reject();
